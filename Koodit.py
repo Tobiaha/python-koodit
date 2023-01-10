e6e4cf64cb45c38653c38f1177f5553365a50b7e
tobiaha <topihammarberg28@gmail.com>
2023-01-10 21:24:04 +0200

    lisäsin uuden rivin

---
+++ Koodit.py
@@ -0,0 +1,12 @@
+import random
+
+print ("Tässä on kolmenumeroinen koodi väliltä 0-9")
+print (random.randint(0,9))
+print (random.randint(0,9))
+print (random.randint(0,9))
+
+print ("Tässä on neljänumeroinen koodi väliltä 1-6")
+print (random.randint(1,6))
+print (random.randint(1,6))
+print (random.randint(1,6))
+print (random.randint(1,6))
