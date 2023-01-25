6588b4339675d87c25d4e0653f973c5a1b461329
tobiaha <topihammarberg28@gmail.com>
2023-01-25 22:06:28 +0200

    lisäsin uuden rivin

---
+++ 6_4kokolukparam.py
@@ -0,0 +1,14 @@
+import math
+def sumnum(summa):
+   # for n in summa:
+
+    luvut = sum({1, 2, 3, 4, 5, 6, 8, 10})
+    summa.append(luvut)
+    return (summa)
+
+
+summa = []
+summa = sumnum(summa)
+print(summa)
+
+
